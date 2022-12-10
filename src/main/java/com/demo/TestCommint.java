19dfa59d35e1d6e1fe26a6202245f0d7de5f17b2
Mthxu <blwbdhl@163.com>
2022-12-10 21:22:03 +0800

    第二次提交

--- src/main/java/com/demo/TestCommint.java
+++ src/main/java/com/demo/TestCommint.java
@@ -3,5 +3,8 @@ package com.demo;
 public class TestCommint {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello2");
+
     }
 }
